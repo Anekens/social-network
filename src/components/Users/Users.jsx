01bbc398e925a1e20dafdea9c35e5d3af19a5052
Pavel Milenki <p.milenkii@gmail.com>
2019-10-18 16:44:49 +0300

    added 51

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,36 +1,16 @@
 import React from 'react'
 import s from './users.module.css'
+import * as axios from 'axios'
+import userPhoto from '../../assets/images/user.png'
 
 let Users = (props) => {
+
     if (props.users.length === 0) {
 
-    props.setUsers([
-            {
-                id: 1,
-                photoUrl: 'http://pp.userapi.com/yuyiUoWX3Vz5J0OEqQ3epUc-ulGnS90jiDG3YQ/YTXzdrfOWAI.jpg?ava=1',
-                followed: false,
-                fullName: 'Dmitry',
-                status: 'I am boss',
-                location: {city: 'Minsk', country: 'Belarus'}
-            },
-            {
-                id: 2,
-                photoUrl: 'http://pp.userapi.com/yuyiUoWX3Vz5J0OEqQ3epUc-ulGnS90jiDG3YQ/YTXzdrfOWAI.jpg?ava=1',
-                followed: true,
-                fullName: 'Sasha',
-                status: 'I am boss too',
-                location: {city: 'Moscow', country: 'Russia'}
-            },
-            {
-                id: 3,
-                photoUrl: 'http://pp.userapi.com/yuyiUoWX3Vz5J0OEqQ3epUc-ulGnS90jiDG3YQ/YTXzdrfOWAI.jpg?ava=1',
-                followed: false,
-                fullName: 'Andrew',
-                status: 'I am boss too',
-                location: {city: 'Kiev', country: 'Ukraine'}
-            },
-        ]
-    );
+    axios.get("https://social-network.samuraijs.com/api/1.0/users"). then(response => {
+
+        props.setUsers(response.data.items);
+    });
 }
 
     return (
@@ -39,7 +19,8 @@ let Users = (props) => {
                 props.users.map(u => <div key={u.id}>
                     <span>
                         <div>
-                            <img src={u.photoUrl} className={s.userPhoto}/>
+                            <img src={u.photos.small != null ? u.photos.small : userPhoto }
+                                 className={s.userPhoto}/>
                         </div>
                         <div>
                             {u.followed
@@ -54,12 +35,12 @@ let Users = (props) => {
                     </span>
                     <span>
                         <span>
-                            <div>{u.fullName}</div>
+                            <div>{u.name}</div>
                             <div>{u.status}</div>
                         </span>
                         <span>
-                            <div>{u.location.country}</div>
-                            <div>{u.location.city}</div>
+                            <div>{"u.location.country"}</div>
+                            <div>{"u.location.city"}</div>
                         </span>
                     </span>
                 </div>)
--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@ import React from 'react';
 import * as serviceWorker from './serviceWorker';
 import store from "./redux/redux-store";
 import ReactDOM from "react-dom";
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, HashRouter} from "react-router-dom";
 import App from "./App";
 import "./index.css"
 import {Provider} from "react-redux";
 
 
 ReactDOM.render(
-    <BrowserRouter>
+    <HashRouter >
         <Provider store={store}>
             <App/>
         </Provider>
-    </BrowserRouter>, document.getElementById('root'));
+    </HashRouter>, document.getElementById('root'));
 
 
 // If you want your app to work offline and load faster, you can change
