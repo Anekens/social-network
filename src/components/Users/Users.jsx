673c8ad31e5a6be9fc66e75433b402adad542a62
Pavel Milenki <p.milenkii@gmail.com>
2019-10-18 17:00:33 +0300

    added 51

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,21 +5,22 @@ import userPhoto from '../../assets/images/user.png'
 
 let Users = (props) => {
 
-    if (props.users.length === 0) {
-
-    axios.get("https://social-network.samuraijs.com/api/1.0/users"). then(response => {
-
-        props.setUsers(response.data.items);
-    });
-}
+    let getUsers = () => {
+        if (props.users.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+                props.setUsers(response.data.items);
+            });
+        }
+    };
 
     return (
         <div>
+            <button onClick={getUsers}>Get users</button>
             {
                 props.users.map(u => <div key={u.id}>
                     <span>
                         <div>
-                            <img src={u.photos.small != null ? u.photos.small : userPhoto }
+                            <img src={u.photos.small != null ? u.photos.small : userPhoto}
                                  className={s.userPhoto}/>
                         </div>
                         <div>
