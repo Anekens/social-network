6be13ba0900feac1d7c3ae9edf67eae321f85b51
Pavel Milenki <p.milenkii@gmail.com>
2019-10-18 15:12:57 +0300

    Updates

---
+++ precache-manifest.45d1b22f0eaa090c21572a3740227810.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48590554c9f9f83ab6be76269b01178c",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "0828e044fc433d5aa29a",
+    "url": "/social-network/static/css/main.c0ca0d4f.chunk.css"
+  },
+  {
+    "revision": "8ef1bc1ebe4a8c1583bd",
+    "url": "/social-network/static/js/2.cbde82ea.chunk.js"
+  },
+  {
+    "revision": "0828e044fc433d5aa29a",
+    "url": "/social-network/static/js/main.635f9370.chunk.js"
+  },
+  {
+    "revision": "fc47203dc7c6eafb9b19",
+    "url": "/social-network/static/js/runtime~main.e65d6286.js"
+  }
+]);
