91cde2776af81c20ebef92585b1f631e854c99f5
Pavel Milenki <p.milenkii@gmail.com>
2019-10-22 01:54:41 +0300

    added 61 login and auth

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import s from './Header.module.css';
+import {NavLink} from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
     return (
         <header className={s.header}>
             <img src="https://seeklogo.com/images/G/globo-logo-486706AB94-seeklogo.com.png"  />
+            <div className={s.loginBlock}>
+                {props.isAuth ? props.login
+                : <NavLink to={'/login'} activeClassName={s.activeLink}>Login</NavLink> }
+            </div>
         </header>
     )
 };
