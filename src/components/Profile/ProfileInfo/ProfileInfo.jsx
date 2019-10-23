32c09efc20ad07d05281bfc7a7fabdd6b181e23e
Pavel Milenki <p.milenkii@gmail.com>
2019-10-23 17:54:46 +0300

    added 71 setState

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import s from './ProfileInfo.module.css';
 import Preloader from "../../Common/Preloader/Preloader";
+import ProfileStatus from "./ProfileStatus";
 
 
 const ProfileInfo = (props) => {
@@ -11,13 +12,14 @@ const ProfileInfo = (props) => {
 
     return (
         <div>
-            <div className={s.image}>
-                <img
-                    src="https://www.mountsinai.org/files/MSHealth/Assets/Media/final/new-york-city-brooklyn-bridge-2.jpg"
-                    alt=""/>
-            </div>
+            {/*<div className={s.image}>*/}
+            {/*    <img*/}
+            {/*        src="https://www.mountsinai.org/files/MSHealth/Assets/Media/final/new-york-city-brooklyn-bridge-2.jpg"*/}
+            {/*        alt=""/>*/}
+            {/*</div>*/}
             <div className={s.descriptionBlock}>
                 <img src={props.profile.photos.large}/>
+               <ProfileStatus status="yoyoyo"/>
                 <h2>Description</h2>
                 <p>{props.profile.aboutMe}</p>
                 <p>{props.profile.contacts.facebook}</p>
