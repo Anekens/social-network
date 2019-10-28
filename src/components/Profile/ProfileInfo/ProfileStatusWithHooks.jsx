bcc842c6e10655d346213462d6c77a9398cce45e
Pavel Milenki <p.milenkii@gmail.com>
2019-10-28 20:20:09 +0300

    added 84 hook

---
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -0,0 +1,44 @@
+import React, {useState} from 'react';
+
+
+const ProfileStatusWithHooks = (props) => {
+
+    const [editMode, setEditMode] = useState(false);
+    const [status, setStatus] = useState(props.status);
+
+    const activateEditMode = () => {
+        setEditMode(true)
+    };
+
+    const deactivateEditMode = () => {
+        setEditMode(false);
+        props.updateStatus(status);
+    };
+
+    const onStatusChange = (e) => {
+        setStatus(e.currentTarget.value)
+    };
+
+    return (
+        <div>
+            {!editMode &&
+            <div>
+                    <span onDoubleClick={activateEditMode}
+                          style={{backgroundColor: '#ffe23b'}}>
+                        {props.status || "no status"}
+                    </span>
+            </div>
+            }
+            {editMode &&
+            <div>
+                <input value={status}
+                       onBlur={deactivateEditMode}
+                       autoFocus={true}
+                       onChange={onStatusChange}/>
+            </div>
+            }
+        </div>
+    )
+};
+
+export default ProfileStatusWithHooks
